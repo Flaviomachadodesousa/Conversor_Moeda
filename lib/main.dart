4f738d4b5b355e77eb5f13bbe02da094fc600857
Flavio <flavio.sos.machado@gmail.com>
2020-02-05 14:26:17 -0300

    s

--- lib/main.dart
+++ lib/main.dart
@@ -112,7 +112,7 @@ class _HomeState extends State<Home> {
                 if (snapshot.hasError) {
                   return Center(
                       child: Text(
-                    "Não carregou ;(",
+                    "Não carregou :(",
                     style: TextStyle(color: Colors.amber, fontSize: 25.0),
                     textAlign: TextAlign.center,
                   ));
